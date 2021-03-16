df121ceb0391b72fe2be8323ddc5acdf91171e36
msuherma <msuherma@purdue.edu>
2021-03-16 16:30:55 -0400

    hw6 not finished

--- notebook/csvToMatrix.py
+++ notebook/csvToMatrix.py
@@ -10,3 +10,10 @@ try:
 except ImportError:
     DATA = Path().resolve() / 'data'
 
+techMat_csv = DATA / 'hw6_tech_matrix.csv'
+fdQ2_csv = DATA / 'hw6_fd_q2.csv'
+fdQ5_csv = DATA / 'hw6_fd_q5.csv'
+
+for entry in DATA.iterdir():
+    if entry.is_file():
+        print(entry.name)
